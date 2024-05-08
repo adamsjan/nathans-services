05051b4b15ea69046a97366d016a21a9e023b4ec
Janely Adams <adamsjan@ut.ee>
2024-05-08 10:28:22 +0300

    database

--- nathanservice/src/components/PostToShow.vue
+++ nathanservice/src/components/PostToShow.vue
@@ -28,17 +28,12 @@ export default {
           }
         );
 
-        console.log("REPONSE", response)
+        console.log("REPONSE", response);
 
         if (response.ok) {
-          const text = await response.text(); // Get response body as text
-  console.log("Response text:", text); // Log text to see what's being received
-  if (text) {
           this.post = await response.json();
-          JSON.parse(text);
           this.$nextTick(() => {});
           console.log("Post get successfully");
-  }
         } else {
           console.error("Failed to get post");
         }
