9fbef1ae34601d9c79a90ee29332c823d10bbb32
Janely Adams <adamsjan@ut.ee>
2024-05-08 09:46:19 +0300

    database

--- nathanservice/src/components/PostToShow.vue
+++ nathanservice/src/components/PostToShow.vue
@@ -17,9 +17,9 @@ export default {
     async getPost() {
       try {
         const baseurl = process.env.FRONTEND_URL;
-        console.log("URL", `${baseurl}`);
+       
         const response = await fetch(
-          `${baseurl}/api/posts/${this.postId}`,
+          `/api/posts/${this.postId}`,
           {
             method: "GET",
             headers: {
