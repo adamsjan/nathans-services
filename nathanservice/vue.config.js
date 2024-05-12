275ec507276a6ac713539eca0088ea2c2e202345
Janely Adams <adamsjan@ut.ee>
2024-05-12 23:43:09 +0300

    images

--- nathanservice/vue.config.js
+++ nathanservice/vue.config.js
@@ -4,3 +4,19 @@ module.exports = defineConfig({
   publicPath: './',
   transpileDependencies: true
 })
+
+module.exports = {
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('purgecss')
+        .use(require('@fullhuman/postcss-purgecss'), [{
+          content: [
+            './public/**/*.html',
+            './src/**/*.vue',
+            './src/**/*.jsx',
+          ],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        }]);
+    }
+  }
+}
