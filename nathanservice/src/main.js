606c74e3e79c0d26c85cae8b1fb7fcbdb288ef98
Janely Adams <adamsjan@ut.ee>
2024-05-13 20:36:10 +0300

    performance

--- nathanservice/src/main.js
+++ nathanservice/src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const router = createRouter();
-
 const app = createApp(App);
 app.use(router);
 router.isReady().then(() => {
