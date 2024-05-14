a1b7ddf4181aac68341bd441084819dd31df2393
Janely Adams <adamsjan@ut.ee>
2024-05-14 23:31:51 +0300

    performance

--- nathanservice/src/views/AdminServicesView.vue
+++ nathanservice/src/views/AdminServicesView.vue
@@ -1,83 +1,103 @@
 <template>
-<div>
-    <AdminHeader></AdminHeader>
-    <main>
-        <div class="row">
-            <div class="column">
-                <div class="card">
-                    <p><span class="material-symbols-outlined">group</span></p>
-                    <h3>11+</h3>
-                    <p>Partners</p>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="card">
-                    <p><span class="material-symbols-outlined">done_all</span></p>
-                    <h3>55+</h3>
-                    <p>Projects</p>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="card">
-                    <p><span class="material-symbols-outlined">sentiment_satisfied</span></p>
-                    <h3>100+</h3>
-                    <p>Happy Clients</p>
-                </div>
-            </div>
-        </div>
-
+  
+    <div>
+      <AdminHeader></AdminHeader>
+  
+      <main>
         <div class="button-container">
-            <button class="cta-button">TELLI TEENUS</button>
+          <a href="#/kontakt" class="button">
+            <svg width="100%" height="100%">
+              <rect x="0" y="0" fill="none" width="100%" height="100%" />
+            </svg>
+            TELLI TEENUS</a
+          >
         </div>
-
+  
         <div class="service-dots-container">
-            <div class="service-icon-container">
-                <span class="dot serviceBtn dot-active" id="Haldus"><span class="material-symbols-outlined">
-                        local_gas_station
-                    </span></span>
-
-                <PostToEdit :postId="18"></PostToEdit>
-            </div>
-            <div class="service-icon-container">
-                <span class="dot serviceBtn" id="Hooldus"><span class="material-symbols-outlined">
-                        manufacturing
-                    </span></span>
-                <PostToEdit :postId="14"></PostToEdit>
-            </div>
-            <div class="service-icon-container">
-                <span class="dot serviceBtn" id="Parandus"><span class="material-symbols-outlined">
-                        handyman
-                    </span></span>
-                <PostToEdit :postId="17"></PostToEdit>
-            </div>
-
+          <div class="service-icon-container">
+            <span class="dot serviceBtn dot-active" id="Haldus"
+              ><div class="icon">
+                <svg viewbox="0 0 24 24" width="24" height="24">
+    <path d="M3 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5z"/>
+    <path d="M1 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v8a2 2 0 0 1 2 2v.5a.5.5 0 0 0 1 0V8h-.5a.5.5 0 0 1-.5-.5V4.375a.5.5 0 0 1 .5-.5h1.495c-.011-.476-.053-.894-.201-1.222a.97.97 0 0 0-.394-.458c-.184-.11-.464-.195-.9-.195a.5.5 0 0 1 0-1q.846-.002 1.412.336c.383.228.634.551.794.907.295.655.294 1.465.294 2.081v3.175a.5.5 0 0 1-.5.501H15v4.5a1.5 1.5 0 0 1-3 0V12a1 1 0 0 0-1-1v4h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1zm9 0a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13h8z"/>
+  </svg></div></span
+            >
+  
+            <p class="service-title">
+              <PostToEdit :postId="14"></PostToEdit>
+            </p>
+          </div>
+          <div class="service-icon-container">
+            <span class="dot serviceBtn" id="Hooldus"
+              ><div class="icon">
+                <svg viewbox="0 0 24 24" width="24" height="24">
+    <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018"/>
+    <path d="M13 6.5a6.47 6.47 0 0 1-1.258 3.844q.06.044.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11"/>
+  </svg>
+  </div></span
+            >
+            <p class="service-title">
+              <PostToEdit :postId="16"></PostToEdit>
+            </p>
+          </div>
+          <div class="service-icon-container">
+            <span class="dot serviceBtn" id="Parandus"
+              ><div class="icon">
+                <svg viewbox="0 0 24 24" width="24" height="24">
+    <path d="M3.904 1.777C4.978 1.289 6.427 1 8 1s3.022.289 4.096.777C13.125 2.245 14 2.993 14 4s-.875 1.755-1.904 2.223C11.022 6.711 9.573 7 8 7s-3.022-.289-4.096-.777C2.875 5.755 2 5.007 2 4s.875-1.755 1.904-2.223"/>
+    <path d="M2 6.161V7c0 1.007.875 1.755 1.904 2.223C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777C13.125 8.755 14 8.007 14 7v-.839c-.457.432-1.004.751-1.49.972C11.278 7.693 9.682 8 8 8s-3.278-.307-4.51-.867c-.486-.22-1.033-.54-1.49-.972"/>
+    <path d="M2 9.161V10c0 1.007.875 1.755 1.904 2.223C4.978 12.711 6.427 13 8 13s3.022-.289 4.096-.777C13.125 11.755 14 11.007 14 10v-.839c-.457.432-1.004.751-1.49.972-1.232.56-2.828.867-4.51.867s-3.278-.307-4.51-.867c-.486-.22-1.033-.54-1.49-.972"/>
+    <path d="M2 12.161V13c0 1.007.875 1.755 1.904 2.223C4.978 15.711 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13v-.839c-.457.432-1.004.751-1.49.972-1.232.56-2.828.867-4.51.867s-3.278-.307-4.51-.867c-.486-.22-1.033-.54-1.49-.972"/>
+  </svg></div></span
+            >
+            <p class="service-title">
+              <PostToEdit :postId="18"></PostToEdit>
+            </p>
+          </div>
         </div>
-
+  
         <div class="description active-desc" id="Haldus-description">
-            <div class="description-content">
-                <PostToEdit :postId="18"></PostToEdit>
-                <PostToEdit :postId="19"></PostToEdit>
-            </div>
+          <div class="left heading trapezoid-left description-content">
+            <h1>
+              <PostToEdit :postId="14"></PostToEdit>
+            </h1>
+          </div>
+          <div class="right">
+            <p class="text-new">
+              <PostToEdit :postId="15"></PostToEdit>
+            </p>
+          </div>
         </div>
+  
         <div class="description" id="Hooldus-description">
-            <div class="description-content">
-                <PostToEdit :postId="14"></PostToEdit>
-                <PostToEdit :postId="20"></PostToEdit>
-            </div>
+          <div class="left">
+            <p class="text-new">
+              <PostToEdit :postId="17"></PostToEdit>
+            </p>
+          </div>
+          <div class="right heading trapezoid-right description-content">
+            <h1>
+              <PostToEdit :postId="16"></PostToEdit>
+            </h1>
+          </div>
         </div>
+  
         <div class="description" id="Parandus-description">
-            <div class="description-content">
-                <PostToEdit :postId="17"></PostToEdit>
-                <PostToEdit :postId="21"></PostToEdit>
-            </div>
+          <div class="left heading trapezoid-left description-content">
+            <h1>
+              <PostToEdit :postId="18"></PostToEdit>
+            </h1>
+          </div>
+          <div class="right">
+            <p class="text-new">
+              <PostToEdit :postId="19"></PostToEdit>
+            </p>
+          </div>
         </div>
-
-    </main>
-    <AdminFooter></AdminFooter>
-</div>
-</template>
+      </main>
+      <AdminFooter></AdminFooter>
+    </div>
+  </template>
 
   
 <script>
@@ -87,255 +107,106 @@ import AdminFooter from '@/components/AdminFooter.vue'
 import PostToEdit from '@/components/PostToEdit.vue'
 
 export default {
-    name: 'HomeView',
+    name: 'AdminServicesView',
     components: {
         AdminFooter,
         AdminHeader,
         PostToEdit
     },
-    mounted() {
-        document.addEventListener('DOMContentLoaded', function () {
-            document.addEventListener('scroll', function () {
-                var header = document.querySelector('.sticky-header');
-                var heroImageHeight = document.querySelector('.logo-container').offsetHeight;
+    beforeMount() {
+    this.updateClasses();
+  },
+  mounted() {
 
-                if (window.scrollY > heroImageHeight * 0.9) { // Adjust this value as needed
-                    header.classList.add('sticky');
-                } else {
-                    header.classList.remove('sticky');
-                }
-            });
+    // Add an event listener to update classes on window resize
+    window.addEventListener("resize", this.updateClasses());
+
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 600) {
+      const buttons = document.querySelectorAll(".serviceBtn");
+      buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+          buttons.forEach((otherButton) => {
+            if (otherButton != button) {
+              document
+                .getElementById(otherButton.id + "-description")
+                .classList.remove("active-desc");
+              otherButton.classList.remove("dot-active");
+            }
+          });
+          document
+            .getElementById(button.id + "-description")
+            .classList.add("active-desc");
+          button.classList.add("dot-active");
         });
+      });
 
-        const buttons = document.querySelectorAll(".serviceBtn");
-        buttons.forEach(button => {
-            button.addEventListener('click', function () {
-                buttons.forEach(otherButton => {
-                    if (otherButton != button) {
-                        document.getElementById(otherButton.id + "-description").classList.remove('active-desc');
-                        otherButton.classList.remove('dot-active');
-                    }
-                })
-                console.log(button.id + "-description");
-                document.getElementById(button.id + "-description").classList.add('active-desc');
-                button.classList.add('dot-active');
-            })
-
+      // Create the observer
+      const observerForLeftRight = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // It's visible. Add the animation class here!
+            entry.target.querySelector(".left").classList.add("left-animation");
+            entry.target
+              .querySelector(".right")
+              .classList.add("right-animation");
+          } else {
+            entry.target
+              .querySelector(".left")
+              .classList.remove("left-animation");
+            entry.target
+              .querySelector(".right")
+              .classList.remove("right-animation");
+          }
         });
-    },
-    methods: {},
+      });
+
+      // Tell the observer which elements to track
+      observerForLeftRight.observe(
+        document.querySelector("#Haldus-description")
+      );
+      observerForLeftRight.observe(
+        document.querySelector("#Hooldus-description")
+      );
+      observerForLeftRight.observe(
+        document.querySelector("#Parandus-description")
+      );
+    }
+  },
+  methods: {
+    updateClasses() {
+      const elements = document.querySelectorAll(".description");
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth >= 600) {
+        elements.forEach((element) => {
+          if (element === elements[1]) {
+            element.children[1].className = "right heading trapezoid-right";
+            element.children[0].className = "left";
+          } else {
+            element.children[0].className = "left heading trapezoid-left";
+            element.children[1].className = "right";
+          }
+        });
+      } else {
+        elements.forEach((element) => {
+          if (element === elements[1]) {
+            element.children[1].className = "divider-section heading";
+            element.children[0].className = "who-are-we";
+          } else {
+            element.children[0].className = "divider-section heading";
+            element.children[1].className = "who-are-we";
+          }
+        });
+      }
+    }
+  }
 }
 </script>
 
-  
 <style scoped>
-:root {
-    /* --blue: rgb(58, 82, 182); */
-    --dark: #4f9290;
-    --light: #BBD5D3;
-    --white: #ffffff;
-    --bold: #10d1cb;
-}
 
-.active {
-    text-decoration: underline;
-}
+  @import '../assets/services.css';
 
-main {
-    height: fit-content;
-
-    margin-top: 5%;
-    margin-bottom: 10%;
-}
-
-.button-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 5%;
-}
-
-.cta-button {
-    background-color: var(--bold);
-    color: white;
-    box-shadow: none;
-    border: none;
-    padding: 2%;
-    border-radius: 5px;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    font-size: 14px;
-    font-weight: bold;
-}
-
-.cta-button:hover {
-    background-color: var(--dark);
-    cursor: pointer;
-}
-
-.service-dots-container {
-    width: 100%;
-    padding: 0% 10%;
-    display: flex;
-    justify-content: space-evenly;
-}
-
-.service-icon-container {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-.dot {
-    margin: 2%;
-    height: 80px;
-    width: 80px;
-    background-color: var(--light);
-    border-radius: 50%;
-    display: inline-block;
-    transition: transform 0.2s ease;
-    /* Smooth transition */
-}
-
-.dot:hover {
-    background-color: var(--dark);
-    cursor: pointer;
-    animation: jump 1s ease;
-}
-
-.dot-active {
-    background-color: var(--dark);
-    cursor: pointer;
-}
-
-@keyframes jump {
-
-    0%,
-    100% {
-        transform: translateY(0);
-    }
-
-    50% {
-        transform: translateY(-20px);
-        /* Adjust for desired height */
-    }
-}
-
-.material-symbols-outlined {
-    margin: 20% 20%;
-    font-weight: 100;
-
-    font-size: 50px;
-    color: #fff;
-}
-
-.service-title {
-    color: #73A9A5;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
-}
-
-.description {
-    height: 50%;
-    width: 70%;
-    margin: auto;
-    margin-top: 5%;
-    margin-bottom: 5%;
-    background-color: var(--light);
-    border-radius: 2em;
-    display: none;
-    padding: 2% 2%;
-    text-decoration: none;
-}
-
-.description-content {
-    height: 100%;
-    width: 100%;
-}
-
-textarea {
-    resize: none;
-    width: 100%;
-    height: 100%;
-}
-
-.active-desc {
-    display: block;
-}
-
-
-/* Float four columns side by side */
-.column {
-
-width: 33%;
-padding: 0% 5%;
-}
-
-.row {
-margin: 7% 0%;
-display: flex;
-flex-direction: row;
-align-items: center;
-}
-
-/* Clear floats after the columns */
-.row:after {
-content: "";
-display: table;
-clear: both;
-}
-
-/* Responsive columns */
-@media screen and (max-width: 600px) {
-.column {
-    width: 100%;
-    display: block;
-    margin-bottom: 10px;
-}
-}
-
-/* Style the counter cards */
-.card {
-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-padding: 16px;
-text-align: center;
-background-color: var(--darker);
-color: white;
-}
-
-.card>p {
-color: white;
-}
-
-.fa {
-font-size: 60px;
-}
-
-@media (max-width: 600px) {
-.row {
-    flex-direction: column;
-}
-.column {
-    margin: 10%;
-    width: 70%;
-}
-.button-container {
-    margin-bottom: 20%;
-}
-.cta-button {
-    padding: 5% 5%;
-    width: 40%;
-    text-align: center;
-    text-decoration: none;
-}
-.service-dots-container {
-    display: none;
-}
-.description {
-    display: flex;
-    flex-direction: column;
-    margin: 15%;
-    padding: 5% 5%;
-}
-}
 </style>
