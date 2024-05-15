5c1fa4c88622cfe6806fa5977431fda6404eb1fe
Janely Adams <adamsjan@ut.ee>
2024-05-15 21:06:53 +0300

    admin

--- nathanservice/src/views/AdminPreviousWorkView.vue
+++ nathanservice/src/views/AdminPreviousWorkView.vue
@@ -8,21 +8,21 @@
   
       <div class="images">
         <div class="content">
-          <PortfolioWork :gallery="'first'" :ids="[20,6]"></PortfolioWork>
+          <PortfolioWork :gallery="'first'" :edit="true" :ids="[20,6]"></PortfolioWork>
 
-        <PortfolioModal :gallery="'first'" :ids="[20, 21, 41, 42, 6,7]"></PortfolioModal>
+        <PortfolioModal :gallery="'first'" :edit="true" :ids="[20, 21, 41, 42, 6,7]"></PortfolioModal>
         </div>
   
         <div class="content">
-          <PortfolioWork :gallery="'second'" :ids="[22,8]"></PortfolioWork>
+          <PortfolioWork :gallery="'second'" :edit="true" :ids="[22,8]"></PortfolioWork>
 
-        <PortfolioModal :gallery="'second'" :ids="[22, 23,43, 44,8,9]"></PortfolioModal>
+        <PortfolioModal :gallery="'second'" :edit="true" :ids="[22, 23,43, 44,8,9]"></PortfolioModal>
         </div>
   
         <div class="content">
-          <PortfolioWork :gallery="'third'" :ids="[24,10]"></PortfolioWork>
+          <PortfolioWork :gallery="'third'" :edit="true" :ids="[24,10]"></PortfolioWork>
 
-          <PortfolioModal :gallery="'third'" :ids="[24, 25,45,46,10,11]"></PortfolioModal>
+          <PortfolioModal :gallery="'third'" :edit="true"  :ids="[24, 25,45,46,10,11]"></PortfolioModal>
         </div>
       </div>
     </main>
